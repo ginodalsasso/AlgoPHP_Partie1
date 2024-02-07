7f9b44b269fcb23c771e3fe75d71af1008a13f1c
kame-sennin23 <dalsasso.gino@gmail.com>
2024-02-07 10:21:24 +0100

    firstcommit

--- exo1.php
+++ exo1.php
@@ -1,9 +1,11 @@
 <h1>Exercice 1</h1>
 
-<p>Enoncé de l'exercice</p>
+<p>Soit la phrase « Notre formation DL commence aujourd'hui ».
+Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase 
+(espaces inclus). </p>
 
 <?php
 
-$phrase = "Comment ça va ?";
-$nbCaracteres = strlen($phrase);
-echo "La phrase contient $nbCaracteres caractères";
+$phrase = "Notre formation DL commence aujourd'hui";
+$nbCaractere = strlen($phrase);
+echo "La phrase contient $nbCaractere caractères";
