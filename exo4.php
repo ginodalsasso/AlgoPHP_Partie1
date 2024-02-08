e82215179efe87ca425fc1d944dbe217f7924919
kame-sennin23 <159161313+kame-sennin23@users.noreply.github.com>
2024-02-08 14:06:12 +0000

    2commitComment

--- exo4.php
+++ exo4.php
@@ -2,17 +2,26 @@
 
 <p>Ecrire un algorithme permettant de savoir si une phrase est palindrome. </p>
 
-<?php 
+<?php
 
-$string= "Engage le jeu que je le gagne";
+$string = "Engage le jeu que je le gagne";
 
-function Palindrome($string){  
-    if (strrev($string) == $string){  
-        return "n'est pas un palindrome";  
-    }
-    else{
-        return "est palindrome";
-    }
-} 
+function estPalindrome($string)
+{
 
-echo $string. " " .Palindrome($string);
+    $stringLower = strtolower($string); // Convertir la phrase en minuscules
+
+    $stringReplaced = str_replace(' ', '', $stringLower); // Supprime les espaces
+
+    $stringReverse = strrev($stringReplaced); // Inverse la phrase
+
+    return $stringReplaced === $stringReverse;
+}
+
+
+if (estPalindrome($string)) {
+    echo 'La phrase "' . $string . '" est palindrome.';
+} else {
+    echo 'La phrase "' . $string . '" n\'est pas palindrome.';
+}
+?>
