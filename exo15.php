03dc78c9eb5d969b0a30a9cde6e0aebc915808b8
kame-sennin23 <dalsasso.gino@gmail.com>
2024-02-12 11:50:59 +0100

    commitClean

--- exo15.php
+++ exo15.php
@@ -12,51 +12,54 @@ Alice DUCHEMIN a … ans </p>
 
 <?php
 
-class Personne{
-    private $_nom;
-    private $_prenom;
-    private DateTime $_birthDate;   // DateTime supprimé ici
+class Personne {
+    private string $_nom;
+    private string $_prenom;
+    private DateTime $_birthDate;
 
-
-    function __construct(string $nom, string $prenom, string $birthDate){
+    function __construct(string $nom, string $prenom, string $birthDate) {
         $this->_nom = $nom;
-        $this->prenom = $prenom;
-        $this->birthDate = new DateTime($birthDate);    // transforme notre "string" en "DateTime"
+        $this->_prenom = $prenom;
+        $this->_birthDate = new DateTime($birthDate);           // transforme notre "string" en "DateTime"
     }
-    
 
-public function getNom(){
-    return $this->_nom;
+    public function getNom() {
+        return $this->_nom;
+    }
+
+    public function setNom(string $nom) {
+        $this->_nom = $nom;
+    }
+
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+
+    public function setPrenom(string $prenom) {
+        $this->_prenom = $prenom;
+    }
+
+    public function getBirthDate() {
+        return $this->_birthDate;
+    }
+
+    public function setBirthDate(string $birthDate) {
+        $this->_birthDate = new DateTime($birthDate);
+    }
+
+    public function getAge() {
+        $now = new DateTime();
+        $diff = $this->_birthDate->diff($now);
+        return $diff->y;                             // ou return $diff->format("%Y") pour renvoyer que le nombre d'années
+    }
+
+    public function __toString() {
+        return $this->_prenom . " " . $this->_nom . " à " . $this->getAge() . " ans";
+    }
 }
 
-public function setNom(string $nom){
-    $this->_nom = $nom;
-}
-public function getPrenom(){
-    return $this->_prenom;
-}
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
 
-public function setPrenom(string $prenom){
-    $this->_nom = $prenom;
-}
-
-public function getBirthDate(){
-    return $this->birthdate;
-}
-
-public function setBirthDate(string $birthdate){
-    $this->_birthdate = $birthDate;
-}
-
-public function getAge() {                              //fonction permettant de calculer l'âge des personnes à partir de la date du jour
-    $now = new DateTime(); // date du jour
-    $diff = $this->birthDate->diff($now);
-return $diff->y;                                         // ou return $diff->format("%Y") pour renvoyer que le nombre d'années
-}
-}
-
-$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
-$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;
-
-echo $p1->getPrenom() . " " . $p1->getNom() . " a " . $p1->getAge() . " ans</br>";
-echo $p2->getPrenom() . " " . $p2->getNom() . " a " . $p2->getAge() . " ans";
+echo $p1."</br>";
+echo $p2;
